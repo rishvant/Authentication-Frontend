c4313d831124b998be3710c673eefc2151757f4a
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-01-23 21:22:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
 function App() {
   return (
     <>
       <div className="cont">
         <Link to="/login">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className="login-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Sign In
           </button>
         </Link>
